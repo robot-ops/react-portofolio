e2ccb02c5dc69ad001d25b80a8cdeedd9c9cea7a
robot-ops <endugbandhel@gmail.com>
2022-12-20 18:09:33 +0700

    chore: set-up hero section

---
+++ src/components/pages/home/home.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import './home.css';
+import Typed from "react-typed";
+import 'react-typed/dist/animatedCursor.css';
+
+function Home() {
+    return (
+        <section id="hero" class="d-flex flex-column justify-content-center align-items-center">
+            <div class="hero-container" data-aos="fade-in">
+            <h1>Aditya Bayu Aji</h1>
+            <p>I'm <span>
+                <Typed className="job-reference"
+                    strings={['UI Designer','Developer','Freelancer','IT Support']}
+                    loop
+                    typeSpeed= {100}
+                    backSpeed= {50}
+                    backDelay= {2000}
+                />
+                </span></p>
+            </div>
+        </section>
+    );
+}
+
+export default Home;
